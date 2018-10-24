4ae060adcab1775ed0f959017020e677f83ab951
Fraser <28698343+FrazzIe@users.noreply.github.com>
2018-10-24 15:34:13 +0100

    created base files, started on config

---
+++ shared.lua
@@ -0,0 +1,36 @@
+Garages = {}
+
+Garages.Data = {
+	Vehicles = {},
+	Garages = {},
+	Price = {
+		Slot = 250,
+		Impound = 5000,
+	},
+}
+
+Garages.Config = {
+	Blips = {
+		["Vehicle"] = {
+			Name = "Garage",
+			Sprite = 1,
+			Colour = 1,
+		},
+		["Watercraft"] = {
+			Name = "Marina",
+			Sprite = 1,
+			Colour = 1,
+		},
+		["Aircraft"] = {
+			Name = "Hangar",
+			Sprite = 1,
+			Colour = 1,
+		},
+	}
+}
+
+Garages.Config.Locations = {
+	["Vehicle"] = {},
+	["Watercraft"] = {},
+	["Aircraft"] = {},
+}
